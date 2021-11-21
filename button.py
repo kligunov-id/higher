dd03003e7a341a09502cd6c99a0c2a4ece9fc2e2
Kligunov <kligunov.id@phystech.edu>
2021-11-21 20:30:21 +0300

    Rework button.py

--- button.py
+++ button.py
@@ -1,51 +1,94 @@
 import pygame
-import os.path
+from locals import FONT_NAME, Color
+from os import path
 
+class Button:
 
-class Button():
+    FONTSIZE_SMALL = 60
+    FONTSIZE_BIG = 70
+    ANIMATION_SPEED = 0.8
+    FONT_NAME = FONT_NAME
+    COLOR = Color.WHITE
 
-    def __init__(self, image, rect, text, fontname, fontsize, color):
-        # хитбокс
-        self.rect = rect
-        #картинка
-        self.image = image
-        self.image_rect = self.image.get_rect(center=self.rect.center)
-        #текст
-        self.fontname = fontname
-        self.fontsize = fontsize
-        self.color = color
+    def __init__(self, text, center, action=None):
+        """ Initializes new button
+        :param text: Displayed button text
+        :param center: List of coordinates (x, y) of the button text
+        :param action: Function with 0 parameters which will be called on-click
+        """
+        self.center = center
+        self.fontsize = Button.FONTSIZE_SMALL
+        self.action = action
         self.update_text(text)
 
-    def update_text(self, text=''):
+    def render(self, screen: pygame.Surface) -> None:
+        """ Blits button image onto given surface
+        :param screen: pygame.Surface to render button on
+        """
+        screen.blit(self.text_surface, self.text_rect)
+
+    def update_text(self, text="") -> None:
+        """ Redraws button with new fontsize and (optionaly) text
+        :param text: New text
+        """
         if text:
             self.text = text
-        self.font = pygame.font.Font(os.path.join('resources', 'fonts', self.fontname), int(self.fontsize))
-        self.text_surf = self.font.render(self.text, True, self.color)
-        self.text_rect = self.text_surf.get_rect(center=self.rect.center)
+        self.font = pygame.font.Font(path.join('resources', 'fonts', Button.FONT_NAME), int(self.fontsize))
+        self.text_surface = self.font.render(self.text, True, Button.COLOR)
+        self.text_rect = self.text_surface.get_rect(center = self.center)
 
-    def draw(self, screen):
-        '''draws self onto a surface'''
-        screen.blit(self.image, self.image_rect)
-        screen.blit(self.text_surf, self.text_rect)
+    def update(self) -> None:
+        """ Animates button """
+        if self.is_mouse_on():
+            self.fontsize += Button.ANIMATION_SPEED
+        else:
+            self.fontsize -= Button.ANIMATION_SPEED
+        # This checks that fontsize in inside [FONTSIZE_SMALL, FONTSIZE_BIG]
+        self.fontsize = max(Button.FONTSIZE_SMALL, self.fontsize)
+        self.fontsize = min(Button.FONTSIZE_BIG, self.fontsize)
+        # This redraws button image and recalculates rect
+        self.update_text()
 
-    def is_mouse_on(self, pos):
+    def is_mouse_on(self) -> bool:
+        """ Checks if mouse is hovering over the button
+        :returns : True if mouse is hovering over the button
         """
-        Checks if mouse is hovering over the button
-        :param pos: Mouse position (x, y)
+        return self.text_rect.collidepoint(pygame.mouse.get_pos())
+    
+    def handle(self, event: pygame.event.Event) -> None:
+        """ Handles mouse clicks
+        :param event: PyGame event to be handled, should be of MOUSEBUTTONDOWN type
         """
-        return self.rect.collidepoint(pos)
+        if event.type != pygame.MOUSEBUTTONDOWN:
+            return
+        if self.action is not None and self.is_mouse_on():
+            self.action()
 
 
-class ButtonFactory:
+if __name__ == '__main__':
+    pygame.init()
+    pygame.font.init()
 
-    def __init__(self, fontname, fontsize, color):
-        self.fontname = fontname
-        self.fontsize = fontsize
-        self.color = color
+    screen = pygame.display.set_mode((600, 600))
 
-    def newbutton(self, imagename:str, pos, size, text:str):
-        image = pygame.image.load(os.path.join('resources', 'images', imagename)).convert()
-        rect = pygame.rect.Rect(pos, size)
-        rect.center = pos
-        button = Button(image, rect, text, self.fontname, self.fontsize, self.color)
-        return button
+    button = Button("Quit", (300, 300), action=exit)
+    clock = pygame.time.Clock()
+    finished = False
+
+    # Main cycle
+    while not finished:
+        clock.tick(60)
+        # Handles events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+            else:
+                button.handle(event)
+
+        button.update()
+        button.render(screen)
+
+        # Updates screen
+        pygame.display.update()
+        screen.fill(Color.BLACK)
+    pygame.quit()
