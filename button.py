b373bc1aff9808020cf963a896026364b5c91cc4
Kligunov <kligunov.id@phystech.edu>
2021-12-18 22:28:34 +0300

    Refactor button.py
    Buttons now trim long entries
    Refactor code to shift responsibilities from ButtonList to Button as it will help to add scrolls

--- button.py
+++ button.py
@@ -2,7 +2,6 @@ import pygame
 from pygame.rect import Rect
 
 from locals import FONT_PATH, Color
-from os import path
 
 """ 
 Implements buttons and keyboard nevigation through menues
@@ -11,7 +10,18 @@ Classes:
 
     Button
     ButtonList
+
+Functions:
+
+   trim(text, max_len=20) -> str
+
 """
+
+def trim(text:str, max_len:int = 30) -> str:
+    """ Trims text"""
+    if len(text) > max_len:
+        text = text[:max_len-3] + "..."
+    return text
 
 class Button:
     text: str
@@ -30,19 +40,30 @@ class Button:
         :param text: Displayed button text
         :param center: List of coordinates (x, y) of the button text
         :param action: Function with 0 parameters which will be called on-click
-        :param keys: Pygame key id to trigger button
+        :param keys: List of Pygame key id-s to trigger button
         """
         self.center = center
         self.fontsize = Button.FONTSIZE_SMALL
         self.action = action
         self.update_text(text)
         self.keys = keys
+        self.active = False
+    
+    def set_active(self, active:bool = True) -> None:
+        """ Activates or deactivates button
+        :param active: True if button should be activated """
+        self.active = active
 
     def render(self, screen: pygame.Surface) -> None:
         """ Blits button image onto given surface
         :param screen: pygame.Surface to render button on
         """
         screen.blit(self.text_surface, self.text_rect)
+        if self.active:
+            # Renders pointer (">") to the active button
+            text_surface = self.font.render("> ", True, Button.COLOR)
+            text_rect = text_surface.get_rect(topright=self.text_rect.topleft)
+            screen.blit(text_surface, text_rect)
 
     def update_text(self, text="") -> None:
         """ Redraws button with new fontsize and (optionaly) text
@@ -51,7 +72,7 @@ class Button:
         if text:
             self.text = text
         self.font = pygame.font.Font(FONT_PATH, int(self.fontsize))
-        self.text_surface = self.font.render(self.text, True, Button.COLOR)
+        self.text_surface = self.font.render(trim(self.text), True, Button.COLOR)
         self.text_rect = self.text_surface.get_rect(center=self.center)
 
     def update(self) -> None:
@@ -77,14 +98,14 @@ class Button:
         :param event: PyGame event to be handled
         """
         should_trigger = ((event.type == pygame.MOUSEBUTTONDOWN and self.is_mouse_on())
-                       or (event.type == pygame.KEYDOWN and self.keys and event.key in self.keys))
+                       or (event.type == pygame.KEYDOWN and self.keys and event.key in self.keys)
+                       or (event.type == pygame.KEYDOWN and self.active and event.key == pygame.K_RETURN))
 
         if should_trigger and self.action is not None:
             self.action()
 
 class ButtonList:
     """ Stores a set of buttons, implements navigation. """
-
 
     def __init__(self, topmid: tuple[int, int], h_step: int):
         """ Initializes empty button list and sets layout
@@ -99,7 +120,9 @@ class ButtonList:
         """ Add already existing button
         :param button: Button instance to add to list
         ..note:: It is preferable to use construct_button()"""
-        self.buttons.append(button) 
+        if not self.buttons:
+            button.set_active()
+        self.buttons.append(button)
     
     def construct_button(self, text: str, action=None, keys=None) -> None:
         """ Creates new button in place
@@ -109,37 +132,36 @@ class ButtonList:
         """
         width, height = self.buttons[-1].center if self.buttons else self.topmid
         height += self.h_step if self.buttons else 0
-        self.buttons.append(Button(text, (width, height), action, keys))
+        new_button = Button(text, (width, height), action, keys)
+        if not self.buttons:
+            new_button.set_active()
+        self.buttons.append(new_button)
 
     def render(self, screen: pygame.Surface) -> None:
-        """ Renders buttons and the pointer
+        """ Renders buttons and scrolls
         :param screen: pygame.Surface to blit on
         """
         for button in self.buttons:
             button.render(screen)
-        # Renders pointer (">") to the active button
-        active_button = self.buttons[self.i]
-        text_surface = active_button.font.render("> ", True, Button.COLOR)
-        text_rect = text_surface.get_rect(topright=active_button.text_rect.topleft)
-        screen.blit(text_surface, text_rect)
 
     def update(self) -> None:
         """ Animates buttons """
         for button in self.buttons:
             button.update()
 
-
     def handle(self, event: pygame.event.Event) -> None:
         """ Handles button navigation and activation through clicks or keystrokes
         :param event: PyGame event to be handled
         """
         if event.type == pygame.KEYDOWN:
+            prev_i = self.i
             if event.key == pygame.K_w or event.key == pygame.K_UP:
                 self.i = (self.i - 1) % len(self.buttons)
             if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 self.i = (self.i + 1) % len(self.buttons)
-            if event.key == pygame.K_RETURN or event.key == pygame.K_SPACE:
-                self.buttons[self.i].action()
+            # Update active button 
+            self.buttons[prev_i].set_active(active=False)
+            self.buttons[self.i].set_active()
         for button in self.buttons:
             button.handle(event)
 
@@ -149,9 +171,10 @@ if __name__ == '__main__':
 
     screen = pygame.display.set_mode((600, 600))
 
-    buttons = ButtonList((300, 300), 100)
-    buttons.construct_button("Quit", action=exit)
-    buttons.construct_button("Quit2", action=exit)
+    buttons = ButtonList((300, 200), 100)
+    buttons.construct_button("Quit", action=exit, keys=[pygame.K_q])
+    buttons.construct_button("Hi", action=lambda:print("Hi"))
+    buttons.construct_button("Chill " * 5)
     clock = pygame.time.Clock()
     finished = False
 
